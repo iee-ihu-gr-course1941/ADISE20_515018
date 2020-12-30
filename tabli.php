aa448eb1d297d5eef03599664631dab380c4dd22
Andreas-Ghikas <74411200+Andreas-Ghikas@users.noreply.github.com>
2020-12-30 18:02:29 +0200

    Create tabli.php

---
+++ tabli.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors','on' );
+
+require_once "db/dbconnect.php";
+// require_once "db/board.php";
+// require_once "db/game.php";
+// require_once "db/users.php";
+
+?>

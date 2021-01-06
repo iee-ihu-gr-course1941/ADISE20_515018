682dd4cc9add84e3b61d42df0a7df771433934a1
Andreas-Ghikas <74411200+Andreas-Ghikas@users.noreply.github.com>
2021-01-06 14:32:23 +0200

    Update mytabli.js

--- js/mytabli.js
+++ js/mytabli.js
@@ -11,19 +11,19 @@ $(function () {
 });
 
 function draw_empty_board(p) {
-	
-	//						For White
-	//		Outer Board			|		Opponent Home
-	//	13	14	15	16	17	18	|	19	20	21	22	23	24
-	//	12	11	10	9	8	7	|	6	5	4	3	2	1
-	//		Outer Board			|		Players Home
-	//
-	//						For Black
-	//		Outer Board			|		Opponent Home
-	//	1	2	3	4	5	6	|	7	8	9	10	11	12
-	//	24	23	22	21	20	19	|	18	17	16	15	14	13
-	//		Outer Board			|		Players Home
-	
+
+   //                            For White
+   //           Outer Board           |          Opponent Home
+   //   13   14   15   16   17   18   |   19   20   21   22   23   24
+   //   12   11   10    9    8    7   |    6    5    4    3    2    1
+   //           Outer Board           |           Players Home
+   //
+   //                            For Black
+   //           Outer Board           |          Opponent Home
+   //    1    2    3    4    5    6   |    7    8    9   10   11   12
+   //   24   23   22   21   20   19   |   18   17   16   15   14   13
+   //           Outer Board           |           Players Home
+
 	if(p!='B') {p='W';}
 	var draw_init = {
 		'W': {sn1:13,sn2:12},
